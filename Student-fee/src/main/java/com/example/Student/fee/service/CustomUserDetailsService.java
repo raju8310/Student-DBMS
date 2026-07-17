3c8cd1d00e345f97c4b849c56a802a10b6f3d4f0
Raju B N <rajubn937@gmail.com>
2026-07-17 22:34:30 +0530

    first commit of this project

---
+++ Student-fee/src/main/java/com/example/Student/fee/service/CustomUserDetailsService.java
@@ -0,0 +1,62 @@
+package com.example.Student.fee.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.Student.fee.entity.Student;
+import com.example.Student.fee.entity.User;
+import com.example.Student.fee.repository.Student_Repository;
+import com.example.Student.fee.repository.User_Repository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final User_Repository userRepository;
+    private final Student_Repository studentRepository;
+
+    public CustomUserDetailsService(User_Repository userRepository,
+    											Student_Repository studentRepository) {
+        this.userRepository = userRepository;
+        this.studentRepository = studentRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String firstName)
+            throws UsernameNotFoundException {
+
+        System.out.println("Login username: " + firstName);
+
+        User user = userRepository.findByFirstName(firstName);
+
+        System.out.println("User from DB: " + user);
+
+
+        if(user!=null) {
+        
+
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getFirstName())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+        }
+        
+        Student student = studentRepository.findByFirstName(firstName);
+
+        if (student != null) {
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(student.getFirstName())
+                    .password(student.getPassword())
+                    .roles("STUDENT")
+                    .build();
+        }
+
+
+        throw new UsernameNotFoundException("User not found");
+    }
+    
+}
---
+++ Student-fee/src/main/java/com/example/Student/fee/service/StudentsService.java
@@ -0,0 +1,27 @@
+package com.example.Student.fee.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.Student.fee.entity.Student;
+
+public interface StudentsService {
+
+	
+
+    public Student saveStudent(Student student);
+
+    List<Student> getAllStudents();
+
+    Student getStudentById(Long id);
+
+    Student updateStudent(Student student);
+
+    void deleteStudent(Long id);
+
+    Student searchStudent(String firstName);
+    
+    public Object getStudentByFirstName(String firstName);
+    
+}
